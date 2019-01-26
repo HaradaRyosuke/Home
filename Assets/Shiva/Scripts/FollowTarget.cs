3a707ee986b34303c244a332bb98f985f480526b
Shinobu Izumi <stagesp1@gmail.com>
2019-01-26 18:28:34 +0900

    更新

--- Assets/Shiva/Scripts/FollowTarget.cs
+++ Assets/Shiva/Scripts/FollowTarget.cs
@@ -1,26 +1,28 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-
-public class FollowTarget : MonoBehaviour
+namespace GGJ2019.Akihabara.Team5
 {
-    public Transform target;
-
-    // Start is called before the first frame update
-    void Start()
+    public class FollowTarget : MonoBehaviour
     {
-        
+        public Transform target;
+
+        // Start is called before the first frame update
+        void Start()
+        {
+
+        }
+
+        // Update is called once per frame
+        void LateUpdate()
+        {
+            if (target)
+                transform.position = Vector3.Lerp(
+                    transform.position,
+                    target.position + Vector3.forward * -10,
+                    Time.deltaTime);
+        }
+
+
     }
-
-    // Update is called once per frame
-    void LateUpdate()
-    {
-        if(target)
-            transform.position = Vector3.Lerp(
-                transform.position, 
-                target.position + Vector3.forward * -10, 
-                Time.deltaTime);
-    }
-
-
 }
